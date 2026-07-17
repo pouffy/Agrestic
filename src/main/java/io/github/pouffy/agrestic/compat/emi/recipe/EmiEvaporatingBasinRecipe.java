c7bb0113c7dca8da0481711edf8df4b7a0b63d7a
PouffyDev <99536749+pouffy@users.noreply.github.com>
2026-07-17 21:33:24 +0100

    bleh

--- src/main/java/io/github/pouffy/agrestic/compat/emi/recipe/EmiEvaporatingBasinRecipe.java
+++ src/main/java/io/github/pouffy/agrestic/compat/emi/recipe/EmiEvaporatingBasinRecipe.java
@@ -1,13 +1,11 @@
 package io.github.pouffy.agrestic.compat.emi.recipe;
 
-import com.mojang.datafixers.util.Either;
 import dev.emi.emi.api.neoforge.NeoForgeEmiIngredient;
 import dev.emi.emi.api.recipe.BasicEmiRecipe;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.SlotWidget;
 import dev.emi.emi.api.widget.WidgetHolder;
-import io.github.pouffy.agrestic.Agrestic;
 import io.github.pouffy.agrestic.common.recipe.EvaporatingBasinRecipe;
 import io.github.pouffy.agrestic.compat.emi.AgresticEmiPlugin;
 import io.github.pouffy.agrestic.compat.emi.FluidTankWidget;
@@ -16,7 +14,6 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.crafting.RecipeHolder;
 
-import java.util.Collections;
 import java.util.List;
 
 public class EmiEvaporatingBasinRecipe extends BasicEmiRecipe {
